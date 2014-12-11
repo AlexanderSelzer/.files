660cdd30c2dde61f8b09a2b17d1a550521c7e99e
Alexander Selzer <aselzer3@gmail.com>
2014-12-11 09:45:37 +0100

    added init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Warning: don't run this if you don't know what it does or if you don't want to replace
+## all your configuration with mine.
+## If you ran it, I am sorry. Please check shell scripts before you run them.
+
+cp .bashrc ~/
+cp .vimrc ~/
